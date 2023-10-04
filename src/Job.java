1fed69a70a097538fb5550547f69040287427557
Damien <d.f.d@me.com>
2023-10-04 14:49:09 -0700

    Updated version of Job

--- src/Job.java
+++ src/Job.java
@@ -7,6 +7,7 @@ public class Job {
     ArrayList<String> proficienciesTypes = new ArrayList<String>();
     //Arraylist index 0 = armor, 1 = weapons, 2 = tools
     ArrayList<String> skills = new ArrayList<String>();
+    ArrayList<String> equipment = new ArrayList<String>();
 
     private Boolean strengthSV;
     private Boolean dexteritySV;
@@ -64,6 +65,10 @@ public class Job {
         return proficienciesTypes;
     }
 
+    ArrayList<String> getEquipment() {
+        return equipment;
+    }
+
     class Barbarian extends Job{
         //Constructor   v   v   v   v   v   v   v   v   v   v   v   v   v   v   v   v   v   v   v   v
 
@@ -79,6 +84,14 @@ public class Job {
             skills.add(input.nextLine());
             System.out.printf("Type the second of the following skills that you'd like,\n*Animal Handling, Athletics, Intimidation, Nature, Perception, Survival:\n");
             skills.add(input.nextLine());
+
+            System.out.printf("Equipment:\n");
+            System.out.printf("type Greataxe or any martial melee weapon:\n");
+            equipment.add(input.nextLine());
+            System.out.printf("Two handaxes or any simple weapon:\n");
+            equipment.add(input.nextLine());
+            equipment.add("Explorer’s pack");
+            equipment.add("Four javalins");
         }
 
 
@@ -118,6 +131,16 @@ public class Job {
             skills.add(input.nextLine());
             System.out.printf("Type the third skill:\n");
             skills.add(input.nextLine());
+
+            System.out.printf("Equipment:\n");
+            System.out.printf("type rapier, longsword, or any simple weapon:\n");
+            equipment.add(input.nextLine());
+            System.out.printf("Diplomat’s pack or an entertainer's pack:\n");
+            equipment.add(input.nextLine());
+            System.out.printf("Lute or any other musical instrument:\n");
+            equipment.add(input.nextLine());
+            equipment.add("Leather armor");
+            equipment.add("Dagger");
         }
     }
 
@@ -136,6 +159,19 @@ public class Job {
             skills.add(input.nextLine());
             System.out.printf("Type the second skill:\n");
             skills.add(input.nextLine());
+
+            System.out.printf("Equipment:\n");
+            System.out.printf("Type mace, or warhammer (if proficient):\n");
+            equipment.add(input.nextLine());
+            System.out.printf("Scale mail, leather armor, or chain mail (if proficient):\n");
+            equipment.add(input.nextLine());
+            System.out.printf("light crossbow and 20 bolts or any simple weapon:\n");
+            equipment.add(input.nextLine());
+            System.out.printf("Priest’s pack or an explorer’s pack:\n");
+            equipment.add(input.nextLine());
+            System.out.printf("Holy symbol:\n");
+            equipment.add(input.nextLine());
+            equipment.add("Shield");
 
         }
     }
@@ -156,6 +192,15 @@ public class Job {
             skills.add(input.nextLine());
             System.out.printf("Type the second skill:\n");
             skills.add(input.nextLine());
+
+            System.out.printf("Equipment:\n");
+            System.out.printf("Type wooden shield or any simple weapon:\n");
+            equipment.add(input.nextLine());
+            System.out.printf("Scimitar or any simple melee weapon:\n");
+            equipment.add(input.nextLine());
+            equipment.add("Leather armor");
+            equipment.add("Explorers pack");
+            equipment.add("Druidic focus");
         }
     }
 
@@ -168,6 +213,7 @@ public class Job {
             baseHP = 10;
             proficienciesTypes.set(0, "All armor, shields");
             proficienciesTypes.set(1, "Simple weapons, martial weapons");
+            String in = "";
 
             Scanner input = new Scanner(System.in);
             System.out.printf("Type the first of two skills,\nAcrobatics, Animal Handling, Athletics, History, Insight, Intimidation, Perception, and Survival:\n");
@@ -175,6 +221,24 @@ public class Job {
             System.out.printf("Type the second skill:\n");
             skills.add(input.nextLine());
 
+            System.out.printf("Equipment:\n");
+            System.out.printf("Type chain mail or B for leather armor, longbow, and 20 arrows:\n");
+            in = input.nextLine();
+            if (in == "b")
+            {
+                equipment.add("leather armor");
+                equipment.add("long bow");
+                equipment.add("20 arrows");
+            } else {
+                equipment.add(input.nextLine());
+            }
+
+            System.out.printf("A martial weapon and a shield or two martial weapons:\n");
+            equipment.add(input.nextLine());
+            System.out.printf("A light crossbow and 20 bolts, or two handaxes:\n");
+            equipment.add(input.nextLine());
+            System.out.printf("Dungeoneer’s pack or (b) an explorer’s pack:\n");
+            equipment.add(input.nextLine());
         }
     }
 
@@ -193,6 +257,13 @@ public class Job {
             skills.add(input.nextLine());
             System.out.printf("Type the second skill:\n");
             skills.add(input.nextLine());
+
+            System.out.printf("Equipment:\n");
+            System.out.printf("type shortsword or any simple weapon:\n");
+            equipment.add(input.nextLine());
+            System.out.printf("Dungeoneer’s pack or an explorer’s pack:\n");
+            equipment.add(input.nextLine());
+            equipment.add("10 Darts");
         }
     }
 
@@ -205,12 +276,36 @@ public class Job {
             baseHP = 10;
             proficienciesTypes.set(0, "all armor, shields");
             proficienciesTypes.set(1, "simple weapons, martial weapons");
+            String in;
 
             Scanner input = new Scanner(System.in);
             System.out.printf("Type the first of two skills,\nAthletics, Insight, Intimidation, Medicine, Persuasion, and Religion:\n");
             skills.add(input.nextLine());
             System.out.printf("Type the second skill:\n");
             skills.add(input.nextLine());
+
+            System.out.printf("Equipment:\n");
+            System.out.printf("Type a martial weapon then shield or B for two martial weapons:\n");
+            in = input.nextLine();
+            if (in == "b")
+            {
+                System.out.printf("Martial weapon 1:\n");
+                equipment.add(input.nextLine());
+                System.out.printf("Martial weapon 2:\n");
+                equipment.add(input.nextLine());
+            } else {
+                equipment.add(input.nextLine());
+                equipment.add(input.nextLine());
+            }
+
+            System.out.printf("Five javelins or any simple melee weapon:\n");
+            equipment.add(input.nextLine());
+            System.out.printf("A priest’s pack or an explorer’s pack:\n");
+            equipment.add(input.nextLine());
+            System.out.printf("Dungeoneer’s pack or (b) an explorer’s pack:\n");
+            equipment.add(input.nextLine());
+            equipment.add("Chainmail");
+            equipment.add("Holy symbol");
         }
     }
 
@@ -231,6 +326,27 @@ public class Job {
             skills.add(input.nextLine());
             System.out.printf("Type the third skill:\n");
             skills.add(input.nextLine());
+
+            String in;
+            System.out.printf("Equipment:\n");
+            System.out.printf("Type two shortswords or b for two simple melee weapons:\n");
+            in = input.nextLine();
+            if (in == "b")
+            {
+                System.out.printf("Simple weapon 1:\n");
+                equipment.add(input.nextLine());
+                System.out.printf("Simple weapon 2:\n");
+                equipment.add(input.nextLine());
+            } else {
+                equipment.add("Shortsword");
+                equipment.add("Shortsword");
+            }
+
+            System.out.printf("Type scale mail or leather armor:\n");
+            equipment.add(input.nextLine());
+            System.out.printf("Dungeoneer’s pack or an explorer’s pack:\n");
+            equipment.add(input.nextLine());
+            equipment.add("longbow + quiver of 20 arrows");
         }
     }
 
@@ -254,6 +370,17 @@ public class Job {
             skills.add(input.nextLine());
             System.out.printf("Type the fourth skill:\n");
             skills.add(input.nextLine());
+
+            System.out.printf("Equipment:\n");
+            System.out.printf("Type rapier or a shortsword:\n");
+            equipment.add(input.nextLine());
+            System.out.printf("Shortbow and 20 arrows or a shortsword:\n");
+            equipment.add(input.nextLine());
+            System.out.printf("Burglar’s pack, a dungeoneer’s pack, or an explorer’s pack:\n");
+            equipment.add(input.nextLine());
+            equipment.add("Leather armor");
+            equipment.add("Two daggers");
+            equipment.add("Theives tools");
         }
     }
 
@@ -271,6 +398,24 @@ public class Job {
             skills.add(input.nextLine());
             System.out.printf("Type the second skill:\n");
             skills.add(input.nextLine());
+
+            String in;
+            System.out.printf("Equipment:\n");
+            System.out.printf("Type any simple weapon or b for a light crossbow and 20 bolts:\n");
+            in = input.nextLine();
+            if (in == "b")
+            {
+                equipment.add("Light crossbow");
+                equipment.add("20 Bolts");
+            } else {
+                equipment.add(in);
+            }
+
+            System.out.printf("Type component pouch or an arcane focus:\n");
+            equipment.add(input.nextLine());
+            System.out.printf("Dungeoneer’s pack or an explorer’s pack:\n");
+            equipment.add(input.nextLine());
+            equipment.add("Two daggers");
         }
         }
 
@@ -290,6 +435,28 @@ public class Job {
             skills.add(input.nextLine());
             System.out.printf("Type the second skill:\n");
             skills.add(input.nextLine());
+
+            String in;
+            System.out.printf("Equipment:\n");
+            System.out.printf("Type any simple weapon or b for a light crossbow and 20 bolts:\n");
+            in = input.nextLine();
+            if (in == "b")
+            {
+                equipment.add("Light crossbow");
+                equipment.add("20 Bolts");
+            } else {
+                equipment.add(in);
+            }
+
+            System.out.printf("Type component pouch or an arcane focus:\n");
+            equipment.add(input.nextLine());
+            System.out.printf("scholar’s pack or a dungeoneer’s pack:\n");
+            equipment.add(input.nextLine());
+            System.out.printf("Type any simple weapon:\n");
+            equipment.add(input.nextLine());
+            equipment.add("Two daggers");
+            equipment.add("Leather armor");
+
         }
     }
 
@@ -307,6 +474,15 @@ public class Job {
             skills.add(input.nextLine());
             System.out.printf("Type the second skill:\n");
             skills.add(input.nextLine());
+
+            System.out.printf("Equipment:\n");
+            System.out.printf("Type quarterstaff or dagger:\n");
+            equipment.add(input.nextLine());
+            System.out.printf("Type component pouch or an arcane focus:\n");
+            equipment.add(input.nextLine());
+            System.out.printf("scholar’s pack or a explorer’s pack:\n");
+            equipment.add(input.nextLine());
+            equipment.add("Spellbook");
         }
     }
 }
